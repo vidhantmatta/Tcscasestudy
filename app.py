9846ed1e48e8c34063e9652caec2696062c8a351
vidhant <mattavidhant@gmail.com>
2020-06-29 17:51:55 +0530

    Login functioning

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from views.deskExec import deskExec
+from views.pharmacist import pharmacist
+from models import Auth,Patient
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patient.db' 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patient.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
 def login():
     return render_template("login.html")
-    
-    
+
+@app.route('/',methods=['POST'])
+def loginauth():
+
+    if request.method == 'POST':
+        patient_username = request.form['username']
+        patient_pass = request.form['password']
+        alluser = Auth.query.all()
+        for user in alluser:
+            if(user.username == patient_username and user.password == patient_pass):
+                if(user.role == "pharma"):
+                    return render_template("pharmacist/searchpatient.html")
+                if(user.role == "deskExec"):
+                    allPatients = Patient.query.all()
+                    return render_template("/deskExec/home.html", patients= allPatients)
+
+        return redirect("/")
+
 #deskExecutive routes
 app.register_blueprint(deskExec)
 
 #pharmacy routes
+app.register_blueprint(pharmacist)
 
 #diagonistics routes
 
--- models.py
+++ models.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patient.db' 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///patient.db'
 db = SQLAlchemy(app)
+
+
+class Auth(db.Model):
+    aid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return 'User' + str(self.aid)
 
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +33,30 @@ class Patient(db.Model):
     city = db.Column(db.String(20), nullable=False)
     state = db.Column(db.String(20), nullable=False)
     status = db.Column(db.String(20), nullable=False, default="Active")
-    
+
     def __repr__(self):
         return 'Patient' + str(self.id)
+
+
+class Med(db.Model):
+    mid = db.Column(db.Integer,primary_key=True)
+    mname = db.Column(db.String(150), nullable = False , unique=True)
+    quantity = db.Column(db.String(150), nullable = False)
+    Rate = db.Column(db.Integer, nullable = False)
+
+
+
+class Pmed(db.Model):
+    pid = db.Column(db.Integer,db.ForeignKey('patient.id'),primary_key=True)
+    medicineId = db.Column(db.Integer,db.ForeignKey('med.mid'), nullable = False)
+    quant = db.Column(db.String(150), nullable = False)
+    amount = db.Column(db.Integer ,nullable=False)
+    issueDate = db.Column(db.DateTime, default=datetime.now)
+    patient = db.relationship("Patient", backref=backref('patients') )
+    med = db.relationship("Med", backref=backref('medicines') )
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
