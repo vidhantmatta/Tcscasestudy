6178090a556ebe8d6e53d43cd5769abcf8b476c9
Sagar Gupta <sagargupta123kr@gmail.com>
2020-07-01 21:04:11 +0530

    Changes by Tanuj

--- app/views/deskExec.py
+++ app/views/deskExec.py
@@ -1,5 +1,5 @@
 from app import app,render_template, redirect, request, flash
-from models import db, Patient
+from models import db, Patient, Med, Pmed, Diagnosistests, Patientdiagnostic
 from flask_login import login_required, current_user
 
 @app.route("/home")
@@ -19,12 +19,18 @@ def patients():
 
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
-def new_post():
+def new_patient():
     if(current_user.role != 'desk'):
         return render_template('auth/accessDenied.html')
 
     if request.method == 'POST':
         patient_ssnId = request.form['ssnId']
+        if(not len(patient_ssnId)==9 or not patient_ssnId.isdigit()):
+            flash("ssnId must be a 9 digit number" , "danger")
+            return redirect('/create')
+        if(Patient.query.filter_by(ssnId=patient_ssnId).first()):
+            flash("Patient with the same ssnId already exists in the database" , "danger")
+            return redirect('/home')
         patient_name = request.form['name']
         patient_age = request.form['age']
         patient_admissionDate = request.form['admissionDate']
@@ -32,11 +38,15 @@ def new_post():
         patient_address = request.form['address']
         patient_city = request.form['city']
         patient_state = request.form['state']
+        if(len(patient_name)==0 or len(str(patient_age))==0 or len(patient_admissionDate)==0 or len(patient_bedType)==0 
+        or len(patient_address)==0 or len(patient_city)==0 or len(patient_state)==0):
+            flash("No field must be empty" , "danger")
+            return redirect('/create')
         newPatient = Patient(ssnId=patient_ssnId, name=patient_name, age=patient_age, admissionDate=patient_admissionDate,
         bedType =patient_bedType, address=patient_address, city=patient_city, state=patient_state)
         db.session.add(newPatient)
         db.session.commit()
-        flash('Patient created')
+        flash('Patient created','success')
         return redirect('/patients')
     else:
         return render_template('deskExec/newPatient.html')
@@ -52,7 +62,7 @@ def search():
         searched_ssnId = request.form['ssnId']
         patient = Patient.query.filter_by(ssnId=searched_ssnId).first()
         if not patient:
-            flash('No patients found with given ssnID')
+            flash('No patients found with given ssnID','danger')
             if(current_user.role == 'desk'):
                 return redirect('/home')
             elif(current_user.role == 'pharm'):
@@ -68,7 +78,7 @@ def delete(id):
     patient = Patient.query.get_or_404(id)
     db.session.delete(patient)
     db.session.commit()
-    flash('Patient deleted')
+    flash('Patient deleted','success')
     return redirect('/patients')
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
@@ -78,17 +88,95 @@ def update(id):
     patient = Patient.query.get_or_404(id)
 
     if request.method == 'POST':
-        patient.ssnId = request.form['ssnId']
-        patient.name = request.form['name']
-        patient.age = request.form['age']
-        patient.admissionDate = request.form['admissionDate']
-        patient.bedType = request.form['bedType']
-        patient.address = request.form['address']
-        patient.city = request.form['city']
-        patient.state = request.form['state']
+        patient_name = request.form['name']
+        patient_age = request.form['age']
+        patient_admissionDate = request.form['admissionDate']
+        patient_bedType = request.form['bedType']
+        patient_address = request.form['address']
+        patient_city = request.form['city']
+        patient_state = request.form['state']
+        if(len(patient_name)==0 or len(str(patient_age))==0 or len(patient_admissionDate)==0 or len(patient_bedType)==0 
+        or len(patient_address)==0 or len(patient_city)==0 or len(patient_state)==0):
+            flash("No field must be empty" , "danger")
+            return render_template('deskExec/update.html', patient=patient)
+        patient.name = patient_name
+        patient.age = patient_age
+        patient.admissionDate = patient_admissionDate
+        patient.bedType = patient_bedType
+        patient.address = patient_address
+        patient.city = patient_city
+        patient.state = patient_state
         db.session.commit()
-        flash('Patient details updated')
+        flash('Patient details updated','success')
         return redirect('/patients')
     else:
         return render_template('deskExec/update.html', patient=patient)
+
+@app.route('/generateBill/<int:id>')
+@login_required
+def generateBill(id):
+    patient = Patient.query.get_or_404(id)
+    medicines = Pmed.query.filter_by(pid=patient.id).all()
+    tests = Patientdiagnostic.query.filter_by(pid=patient.id).all()
+    
+    medAmount=0
+    roomCharge=0
+    testAmount=0
+
+    for med in medicines:
+        medAmount+=med.amount
+
+    for test in tests:
+        testAmount+=test.amount
+        #print(med.med.mname, med.quant, med.amount)
+    if(patient.bedType == "General Ward"):
+        roomCharge = 500
+    elif(patient.bedType == "Semi Sharing"):
+        roomCharge = 1000
+    else:
+        roomCharge = 2000
+    
+    total = medAmount + roomCharge + testAmount
+
+    return render_template('deskExec/generateBill.html', patient=patient, medicines = medicines, tests = tests, 
+    medAmount=medAmount, roomCharge=roomCharge, testAmount=testAmount, total=total)
+
+@app.route("/discharge/<int:id>")
+@login_required
+def discharge(id):
+    if(current_user.role != 'desk'):
+        return render_template('auth/accessDenied.html')
+
+    patient = Patient.query.get_or_404(id)
+    medicines = Pmed.query.filter_by(pid=patient.id).all()
+    tests = Patientdiagnostic.query.filter_by(pid=patient.id).all()
+
+    #deleting medicines for current session after discharge
+    for medicine in medicines:
+        db.session.delete(medicine)
+
+    for test in tests:
+        db.session.delete(test)
+
+    patient.status = "Discharged"
+    db.session.commit()
+
+    flash('Patient discharged','success')
+    return redirect('/patients')
+    
+
+@app.route("/reactivate/<int:id>")
+@login_required
+def reactivate(id):
+    if(current_user.role != 'desk'):
+        return render_template('auth/accessDenied.html')
+    
+    patient = Patient.query.get_or_404(id)
+    patient.status = "Active"
+    db.session.commit()
+
+    flash('Patient status activated','success')
+    return redirect('/patients')
+
+
 
